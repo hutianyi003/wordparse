3977f40d7df808367c379dca5d81cf5bcb8cc419
Hu Tianyi <hutianyi_003@163.com>
2017-11-14 21:53:36 +0800

    add downhtml

--- exp1/main.cpp
+++ exp1/main.cpp
@@ -1,14 +1,18 @@
 #include <clocale>
 #include "webanalysis.h"
+#include "webdownload.h"
 
 
 int main() {
+	webDownloader downhtml("input\\url.csv", 1);
+	downhtml.getHtml("buffer");
+
 	setlocale(LC_ALL, "chi");
 	//start the project
-	webAnalysis wa(100, "buffer\\page");
-	wa.extractInfo();
-	wa.initDictionary();
-	wa.divideWords();
-	wa.output("output\\result.csv");
+	//webAnalysis wa(100, "buffer\\page");
+	//wa.extractInfo();
+	//wa.initDictionary();
+	//wa.divideWords();
+	//wa.output("output\\result.csv");
 	return 0;
 }
